0d63bdd9a6c9e6cf0f5f838a49d6a2ae672ec852
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-01-20 18:33:52 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,15 +2,33 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+  new_student = self.new
+  new_student.id = row[0]
+  new_student.name = row[1]
+  new_student.grade = row[2]
+  new_student  
   end
 
   def self.all
+    query = <<-TAB
+    SELECT * FROM students
+    TAB
+    DB[:conn].execute(query).map do |row| 
+      self.new_from_db(row)
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
+end
 
   def self.find_by_name(name)
+    query = <<-TAB
+    SELECT * FROM students
+    WHERE name = ?
+    LIMIT 1
+    TAB
+    DB[:conn].execute(query, name).map do |row|
+      self.new_from_db(row)
+    end.first
     # find the student in the database given a name
     # return a new instance of the Student class
   end
@@ -40,4 +58,57 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.all_students_in_grade_9
+    query = <<-TAB
+    SELECT * FROM students
+    WHERE grade = 9
+    TAB
+    DB[:conn].execute(query).map do |row|
+      self.new_from_db(row)
+      end
+  end
+
+  def self.students_below_12th_grade
+    query = <<-TAB
+    SELECT * FROM students
+    WHERE grade < 12
+    TAB
+    DB[:conn].execute(query).map do |row|
+      self.new_from_db(row)
+      end
+  end
+
+  def self.first_X_students_in_grade_10(how_many)
+    query = <<-TAB
+    SELECT * FROM students
+    WHERE grade = 10
+    LIMIT ?
+    TAB
+    DB[:conn].execute(query, how_many).map do |row|
+    self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    query = <<-TAB
+    SELECT * FROM students
+    WHERE grade = 10
+    TAB
+    DB[:conn].execute(query).map do |row|
+    self.new_from_db(row)
+    end.first ### returns first
+  end
+
+  def self.all_students_in_grade_X(how_many)
+    query = <<-TAB
+    SELECT * FROM students
+    WHERE grade = ?
+    TAB
+    DB[:conn].execute(query, how_many).map do |row|
+    self.new_from_db(row)
+    end
+  end
+
 end
+
